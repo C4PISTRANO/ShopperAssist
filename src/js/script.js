e41484359b39cc2020820d55da8ee3a0fd5b5487
RAPHAEL <80759812+C4PISTRANO@users.noreply.github.com>
2023-12-03 14:58:46 -0300

    Create function addItem

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+let grocery = document.getElementById('grocery');
+grocery.addEventListener('submit', addItem);
+function addItem(e){
+  e.preventDefault();
+  let data = this.elements.writeList.value;
+  let list = document.querySelector('ol');
+  let item = document.createElement('li');
+  let text = document.createElement('p');
+
+text.textContent = data;
+this.element.writeList.value = "";
+item.append(text);
+list.append(item);
